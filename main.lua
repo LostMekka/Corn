bcc3606afd9754c5c5bdd95d600779e1e3290d1a
twobit <s8572327@gmail.com>
2016-10-15 11:36:17 +0200

    genesis

---
+++ main.lua
@@ -0,0 +1,25 @@
+require "helper"
+
+local G = love.graphics
+
+
+love.mouse.setVisible(false)
+G.setDefaultFilter("nearest", "nearest")
+G.setBackgroundColor(0, 0, 0, 0)
+
+
+
+
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+
+
+end
