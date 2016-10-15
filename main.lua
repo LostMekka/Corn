4d74fe3e85dc029139af1463bf2f953104452320
twobit <s8572327@gmail.com>
2016-10-15 11:45:57 +0200

    draw stuff

--- main.lua
+++ main.lua
@@ -8,7 +8,10 @@ G.setDefaultFilter("nearest", "nearest")
 G.setBackgroundColor(0, 0, 0, 0)
 
 
-
+W = 640
+H = 400
+canvas = G.newCanvas(W, H)
+love.window.setMode(W, H, {})
 
 
 function love.update()
@@ -16,7 +19,25 @@ function love.update()
 end
 
 function love.draw()
+	G.setCanvas(canvas)
 
+
+	-- render your stuff here
+
+	G.clear(10, 10, 10)
+	G.circle("line", 320, 200, 200)
+
+
+
+
+
+
+
+	-- draw canvas independent of resolution
+	G.origin()
+	G.scale(G.getWidth() / W, G.getHeight() / H)
+	G.setCanvas()
+	G.draw(canvas)
 end
 
 function love.keypressed(key)
