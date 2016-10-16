0eb4c7e2277daf6d395291b099a8232181f492ae
Gilbert Röhrbein <gilbert@ifsr.de>
2016-10-16 22:08:23 +0200

    bubble: added animateSwing

--- bubble.lua
+++ bubble.lua
@@ -5,6 +5,13 @@ Bubble = Object:new {
     offsetY = 0,
     alive   = true,
     text    = "Bubble",
+
+    doAnimateSwing = true,
+    animationState = {
+        tick = 0,
+        offsetX = 0,
+        offsetY = 0,
+    },
 }
 
 function Bubble:init()
@@ -12,14 +19,23 @@ end
 
 function Bubble:update()
     if not self.alive then return end
+    if self.doAnimateSwing then self:animateSwing() end
+end
+
+function Bubble:animateSwing()
+    local s = self.animationState
+    s.tick = s.tick + 1
+    --s.offsetX = 1 * math.cos(s.tick * 0.1)
+    s.offsetY = 2 * math.sin(s.tick * 0.08)
 end
 
 function Bubble:draw()
     if not self.alive then return end
+    local s = self.animationState
     G.printf(
         self.text,
-        self.baseXY.x + self.offsetX - self.w/2,
-        self.baseXY.y + self.offsetY,
+        self.baseXY.x + self.offsetX + s.offsetX - self.w/2,
+        self.baseXY.y + self.offsetY + s.offsetY,
         self.w,
         "center")
 end
